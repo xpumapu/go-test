fd1515edb4d430b2ea3d0e926d611408c2d53c6a
Marek <xpumapu@gmail.com>
2016-03-22 08:02:53 +0000

    update server test
    Signed-off-by: Marek <xpumapu@gmail.com>

--- server.go
+++ server.go
@@ -3,28 +3,53 @@ package main
 import "net"
 import "fmt"
 import "bufio"
-import "strings" // only needed below for sample processing
+import "strings" 
+import "os"
+
+const (  
+    CONN_HOST = ""
+    CONN_PORT = "3000"
+    CONN_TYPE = "tcp"
+)
+
 
 func main() {
 
-  fmt.Println("Launching server...")
+  fmt.Println("Start server...")
 
   // listen on all interfaces
-  ln, _ := net.Listen("tcp", ":3000")
-
-  // accept connection on port
-  conn, _ := ln.Accept()
-  fmt.Println("client connected:", conn.RemoteAddr().String())
-
-  // run loop forever (or until ctrl-c)
-  for {
-    // will listen for message to process ending in newline (\n)
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-    // output message received
-    fmt.Print("Message Received:", string(message))
-    // sample process for string received
-    newmessage := strings.ToUpper(message)
-    // send new string back to client
-    conn.Write([]byte(newmessage + "\n"))
+  ln, err := net.Listen(CONN_TYPE, ":" + CONN_PORT)
+  
+  if err != nil {
+    fmt.Println("Listen error:", err.Error())
+    os.Exit(-1)
   }
+
+  defer ln.Close()
+  
+  for {
+    conn, err := ln.Accept()
+    if err != nil {
+      fmt.Println("Accept error:", err.Error())
+      os.Exit(-1)
+    }
+  
+    fmt.Println("client connected:", conn.RemoteAddr().String())
+    
+    go handle_rx(conn)
+  }
+  
+}  
+ 
+ 
+func handle_rx(conn net.Conn) { 
+  // will listen for message to process ending in newline (\n)
+  message, _ := bufio.NewReader(conn).ReadString('\n')
+  // output message received
+  fmt.Print("Message Received (", len(message), ") :", string(message))
+  // sample process for string received
+  newmessage := strings.ToUpper(message)
+  // send new string back to client
+  conn.Write([]byte(newmessage + "\n"))
+  conn.Close()
 }
